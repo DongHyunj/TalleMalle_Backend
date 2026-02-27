d1769b52dba241c7755c4c16f1d02fe8eeaa656d
jdh <wjdehd555@gmail.com>
2026-02-27 12:38:47 +0900

    [Init] 프로젝트 초기 설정

---
+++ src/main/java/org/example/tallemalle_backend/TalleMalleBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.tallemalle_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalleMalleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalleMalleBackendApplication.class, args);
+    }
+
+}
